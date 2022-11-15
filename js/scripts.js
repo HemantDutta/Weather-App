ffe68773538c3039396728e381af1f29f3857fd6
HemantDutta <hemantnorddutta2@gmail.com>
2022-11-15 12:57:50 +0530

    minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 
-
 let searchBtn = document.getElementById('search_btn');
 let city = document.getElementById('city');
 let output = document.getElementById('result');
@@ -21,11 +20,23 @@ let getWeather = () => {
             .then((resp)=> resp.json())
             .then((data)=> {
                 console.log(data);
+
+                if(data.cod === '200') {
+                    
+                }
+
+
+
+                //Catching 404 error
+                if(data.cod === '404') {
+                    output.innerHTML = `<h3 class="text-primary">City not found!</h3>`;
+                }
             })
             .catch(()=>{
-                output.innerHTML = `<h3>City Not Found</h3>`
-            })
+               //Catches Errors
+            });
+            
     }
-}
+};
 
-searchBtn.addEventListener("click", getWeather);
+searchBtn.addEventListener("click", getWeather); //when search button will be clicked, the getWeather function will be executed
